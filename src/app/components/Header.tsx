c01c8316f8a2a2e01606a5dda4e7c5a026dcfdb3
bruno7283 <bruno7283@hotmail.com>
2025-02-03 00:56:59 -0300

    Heading into header

---
+++ src/app/components/Header.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <header
+      className="
+    fixed
+    top-0
+    z-50
+    flex
+  "
+    >
+      Header
+    </header>
+  );
+}
