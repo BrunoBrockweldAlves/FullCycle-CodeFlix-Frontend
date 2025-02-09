1fab7beb4576bd37395b7aae76c29d7cb7a2183d
bruno7283 <bruno7283@hotmail.com>
2025-02-08 21:37:34 -0300

    add hooks

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,9 +1,27 @@
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
 
 export default function Header() {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 0) {
+        setIsScrolled(true);
+      } else {
+        setIsScrolled(false);
+      }
+    };
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
     <header
-      className="
+      className={`
     fixed
     top-0
     z-50
@@ -12,12 +30,13 @@ export default function Header() {
 
     items-center
     justify-between
-    bg-black
+    ${isScrolled && "bg-black"}
     px-4
     py-4
 
     lg:px-10
-    lg:py-4  "
+    lg:py-4
+    transition-all`}
     >
       <div className="flex items-center space-x-4 md:space-x-8">
         <img src="/logo.png" alt="CodeFlix" width={120} height={120} />
