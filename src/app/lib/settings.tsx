972778fd49035aa0b6e7eca9bedde090b3884914
bruno7283 <bruno7283@hotmail.com>
2025-02-11 22:45:34 -0300

    Finished loading lesson

--- src/app/lib/settings.tsx
+++ src/app/lib/settings.tsx
@@ -8,3 +8,19 @@ export const getAppSettings = ():Promise<{theme: string, language: string}> => {
         }, 2000
     )});
 }
+
+export const getUserInfo = ():Promise<{
+    name: string,
+    mail: string,
+    age: number,
+}> => {
+    return new  Promise((resolve) => {
+        setTimeout(() => {
+            resolve({
+                name: 'User-Name',
+                mail: 'user@email.com',
+                age: 30
+            })
+        }, 2000
+    )});
+}
--- src/app/settings/user/page.tsx
+++ src/app/settings/user/page.tsx
@@ -1,7 +1,19 @@
-export default function User(){
+import { getUserInfo } from "src/app/lib/settings"
+
+
+export default async function User(){
+    const {
+        name,
+        mail,
+        age 
+    } = await getUserInfo();
+    
     return (
         <div className="flex flex-col items-center justify-center h-screen">
         <h1 className="text-2xl font-bold">User Page</h1>
+        <div>Name: {name}</div>
+        <div>Mail: {mail}</div>
+        <div>Age: {age}</div>
         </div>
     )
 }
